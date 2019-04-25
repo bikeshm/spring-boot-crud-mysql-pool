b9bc1a18ef6de4f87c2a289a957d37eb9b25b345
Bikesh M <Bikesh.M@taylorandfrancis.com>
2019-04-25 23:22:58 +0530

    created controler - service - dao structor

--- src/main/java/com/mytutor/controllers/MyFirstController.java
+++ src/main/java/com/mytutor/controllers/MyFirstController.java
@@ -1,6 +1,11 @@
 package com.mytutor.controllers;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,21 +14,48 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mytutor.models.UserModel;
 import com.mytutor.services.interfaces.MyServiceInterface;
 
+
 @RestController
 public class MyFirstController {
-	
+
 	@Autowired
 	MyServiceInterface myService;
-	
-	@RequestMapping("/home")     
-    String myHome() {
-        return "Hello World!";
-    }
-	
-	@RequestMapping(value = "/user", method = RequestMethod.POST)     
-    String addUser(@RequestBody UserModel user) {
-        return myService.addUser(user)>0 ? "user added" : "faild";
-    }
-	
-	
+
+	@RequestMapping("/")
+	String myHome(HttpServletResponse response) {
+		
+		response.setStatus(201); // setting custom response code
+		return "Hello World!";
+	}
+
+	@RequestMapping(value = "/user", method = RequestMethod.POST)
+	String addUser(@RequestBody UserModel user) {
+		return myService.addUser(user) > 0 ? "user added" : "faild";
+	}
+
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+	public UserModel getUserById(@PathVariable("id") long id) {
+
+		return myService.getUser(id);
+	}
+
+	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	public List<UserModel> getUsers(@PathVariable("id") long id) {
+
+		return myService.getUsers();
+	}
+
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.PUT)
+	public String updateUser(@PathVariable("id") long id, @RequestBody UserModel user) {
+		return myService.updateUser(id, user) ? "user updated" : "faild";
+	}
+
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
+	public String deleteUser(@PathVariable("id") long id) {
+
+		return myService.deleteUser(id) ? "user delete" : "faild";
+	}
+
 }
+
+
---
+++ src/main/java/com/mytutor/utils/Constants.java
@@ -0,0 +1,6 @@
+package com.mytutor.utils;
+
+public final class Constants {
+    public static final String LIMIT = "";
+
+}
